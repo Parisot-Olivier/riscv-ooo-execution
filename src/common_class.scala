95d1957f72e37962b7580b6c86ced3b4c2a68fd7
Parisot-Olivier <ol.parisot@gmail.com>
2026-08-19 23:06:31 +0200

    Update common_class.scala

--- src/common_class.scala
+++ src/common_class.scala
@@ -8,7 +8,7 @@ object shift_e extends ChiselEnum {
 }
 
 object cond_e extends ChiselEnum {
-    val if_zero, if_true = value
+    val if_zero, if_true, if_not_true = value
 }
 
 class mov_family_bundle extends Bundle {
@@ -22,7 +22,7 @@ class jump_family_bundle extends Bundle {
 }
 
 class ecall_family_bundle extends Bundle {
-
+    //copie de la ROB a l'etat architectural du pc courant au moment de l'allocation
 }
 
 class shift_family_bundle extends Bundle {
@@ -31,10 +31,15 @@ class shift_family_bundle extends Bundle {
 
 
 //assert SIZE_COUNTER_OP > NUMBER_ENTRIES * NUMBER_FIFO ;
-class micro_op_common (SIZE_COUNTER_OP:int, NUMBER_REGS:int, DATA_WIDTH:int) extends Bundle {
+class micro_op_common (SIZE_COUNTER_OP:Int, NUMBER_REGS:Int, DATA_WIDTH:Int, MAX_BRANCH_FLYING: Int) extends Bundle {
 
     val valid = Bool()
 
+    //dès que le decodeur tombe sur un brénchement il marque toutes les instructions qu'il decode en predictive
+    val is_predictive = Bool()
+    //permet de nettoyer en cas de misspredict les bon niveau de branchement
+    val branch_depth = UInt(log2ceil(MAX_BRANCH_FLYING).W)
+    
     val counter_op = UInt(SIZE_COUNTER_OP.W)
 
     val is_mov_family = Bool()
@@ -54,20 +59,20 @@ class micro_op_common (SIZE_COUNTER_OP:int, NUMBER_REGS:int, DATA_WIDTH:int) ext
     val shift_family = new shift_family_bundle
 }
 
-class micro_op_from_decoder (SIZE_COUNTER_OP:int, NUMBER_REGS:int, DATA_WIDTH:int) extends micro_op_common {
+class micro_op_from_decoder (SIZE_COUNTER_OP:Int, NUMBER_REGS:Int, DATA_WIDTH:Int, MAX_BRANCH_FLYING: Int) extends micro_op_common {
     val scr1 = UInt(log2ceil(NUMBER_REGS).W)
     val scr2 = UInt(log2ceil(NUMBER_REGS).W)
     val dst = UInt(log2ceil(NUMBER_REGS).W)
 }
 
-class micro_op_renamed (SIZE_COUNTER_OP:int, NUMBER_REGS:int, DATA_WIDTH:int) extends micro_op_common {
+class micro_op_renamed (SIZE_COUNTER_OP:Int, NUMBER_REGS:Int, DATA_WIDTH:Int, MAX_BRANCH_FLYING: Int) extends micro_op_common {
     val source_physical_reg_1 = UInt(log2ceil(NUMBER_PHYSICAL_REGS).W)
     val source_physical_reg_2 = UInt(log2ceil(NUMBER_PHYSICAL_REGS).W)
     val destination_physical_reg = UInt(log2ceil(NUMBER_PHYSICAL_REGS).W)
 }
 
 
-class micro_op_ready (SIZE_COUNTER_OP:int, NUMBER_PHYSICAL_REGS:int, DATA_WIDTH:int) extends micro_op_renamed {
+class micro_op_ready (SIZE_COUNTER_OP:Int, NUMBER_PHYSICAL_REGS:Int, DATA_WIDTH:Int, MAX_BRANCH_FLYING: Int) extends micro_op_renamed {
     val issue = Bool()
     val is_issue_on_scr1 = Bool()
     val is_issue_on_scr2 = Bool()
