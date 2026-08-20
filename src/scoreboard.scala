da9be9a1209d2e3ad169337fa67b23d61e3ac105
Parisot-Olivier <ol.parisot@gmail.com>
2026-08-20 14:20:51 +0200

    Update scoreboard.scala

--- src/scoreboard.scala
+++ src/scoreboard.scala
@@ -53,7 +53,7 @@ class scoreboard (NUMBER_PHYSICAL_REGS: Int) extends Module {
 
         io.bitmap (i) := buf_bitmap (i)
 
-        //detection RAW
+        //detection dependence non resolue
         when (io.bitmap (i) && !valid_data (i)) { 
             dependency_not_resolved (i) := true.B
         } .otherwise {
@@ -73,28 +73,22 @@ class scoreboard (NUMBER_PHYSICAL_REGS: Int) extends Module {
     buf_micro_op_final.issue := dependency_not_resolved (buf_micro_op.src1) | dependency_not_resolved (buf_micro_op.src2)
 
 
-    val valid_inc = RegInit(false.B)
-    val ecall_inst = RegInit(false.B)
+    val valid_inc_src1 = RegInit(false.B)
+    val valid_inc_src2 = RegInit(false.B)
     
     val inc_annouced_read_src1 = RegInit(0.U(log2ceil(NUMBER_PHYSICAL_REGS).W))
     val inc_annouced_read_src2 = RegInit(0.U(log2ceil(NUMBER_PHYSICAL_REGS).W))
 
     when (buf_micro_op_final.valid) {
-        when (buf_micro_op_final.is_ecall) {
-            ecall_inst := true.B
-            valid_inc := false.B
-        } .otherwise {
-            ecall_inst := false.B
-            valid_inc := true.B
-
-            io.micro_op_final := buf_micro_op_final
+        valid_inc_src1 := buf_micro_op_final.valid_src1
+        valid_inc_src2 := buf_micro_op_final.valid_src2
+        io.micro_op_final := buf_micro_op_final
         
-            //section preuve:
-            //pas de probleme de annouced_read > served_read car au rochain cycle on est sûr 
-            //que l'instruction ne va pas etre executer
-            inc_annouced_read_src1 := buf_micro_op_final.scr1
-            inc_annouced_read_src2 := buf_micro_op_final.scr2
-        }
+        //section preuve:
+        //pas de probleme de annouced_read > served_read car au rochain cycle on est sûr 
+        //que l'instruction ne va pas etre executer
+        inc_annouced_read_src1 := buf_micro_op_final.scr1
+        inc_annouced_read_src2 := buf_micro_op_final.scr2
 
     } .otherwise {
         valid_inc := false.B
@@ -108,21 +102,13 @@ class scoreboard (NUMBER_PHYSICAL_REGS: Int) extends Module {
 //voir preuve 1 dans preuves.md: les deux cas du when sont distincts et annonced_read_counter doit 
 //avoir un seul driver donc un seul when
     for (i <- 0 until NUMBER_PHYSICAL_REGS) {
-        when ((inc_annouced_read_src1 === i | inc_annouced_read_src2 === i) && valid_inc ) {
+        when ((inc_annouced_read_src1 === i && valid_inc_src1) | (inc_annouced_read_src2 === i && valid_inc_src2))  {
             //section preuve:
             //voir preuve 2, pas besoin de gestion du debordement du compteur
             announced_read_counter (i) := announced_read_counter (i) + 1.U 
         } .elsewhen (free_reg (i)) {
             announced_read_counter (i) := 0.U
         } 
-        
-        /* extention ecall: il faut que le registre d'appel soit prêts 
-        if (i==7) { //le registre a7 du ecall
-        .elsewhen (ecall_inst) { 
-            announced_read_counter (i) := announced_read_counter (i) + 1.U 
-        }
-        }
-        */
     }
 }
 
